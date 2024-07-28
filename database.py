1ebf0927b4706d908f3eaba3bd704e8cf9253f95
seth <22097951+Seth2482@users.noreply.github.com>
2024-07-27 23:11:35 -0700

    Fix the bug of invalid mysql connection

--- database.py
+++ database.py
@@ -4,9 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from config import GeneralConfig
 
-engine = create_engine(GeneralConfig.DATABASE_URL, echo=GeneralConfig.DATABASE_LOG)
+engine = create_engine(GeneralConfig.DATABASE_URL, echo=GeneralConfig.DATABASE_LOG,
+                       pool_size=10,
+                       pool_recycle=7200,
+                       pool_timeout=30)
 db_session: scoped_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+                                                         autoflush=True,
+                                                         bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
